7cf0d718add676bc1a06cce3adf9c0399631d5be
Default user <Camiloguti>
2019-10-30 13:30:39 +0000

    ingreso perimetro

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cmath>
 int main(void){
     int N;
     std::cout<<"numero radio a calcular"<<std::endl;
@@ -8,6 +8,10 @@ int main(void){
     int D;
     D=2*N;
     std::cout<<"diametro es"<<D<<std::endl;
+   
+    float P;
+    P=2*M_PI*N;
+    std::cout<<"perimetro es"<<P<<std::endl;
     return 0;
     
 
