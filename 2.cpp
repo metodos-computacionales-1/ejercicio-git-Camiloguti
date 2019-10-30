f3aafdff384abd746e5b608dd9ac1bfdee2a0758
Default user <Camiloguti>
2019-10-30 13:46:22 +0000

    ingreso hipotenusa

---
+++ 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+int main(void){
+    int H;
+    std::cout<<"hipotenusa"<<std::endl;
+    std::cin>>H;
+    std::cout<<"ha ingresado hipotenusa"<<H<<std::endl;
+    
+   
+    
+    
+ 
+    
+    
+    
+    
+    
+return 0;
+    
+    
+
+}
+
+
