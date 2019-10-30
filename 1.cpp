8c8f55413155813b5d61f408a7679ef56514e86d
Default user <Camiloguti>
2019-10-30 13:18:27 +0000

    ingreso radio

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void){
+    int N;
+    std::cout<<"numero radio a calcular"<<std::endl;
+    std::cin>>N;
+    std::cout<<"ha ingresado radio"<<N<<std::endl;
+    return 0;
+    
+
+}
