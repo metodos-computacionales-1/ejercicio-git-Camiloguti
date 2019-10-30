c78979356062868846e755262423aaf1720855e3
Default user <Camiloguti>
2019-10-30 13:24:06 +0000

    ingreso diametro

--- 1.cpp
+++ 1.cpp
@@ -5,6 +5,9 @@ int main(void){
     std::cout<<"numero radio a calcular"<<std::endl;
     std::cin>>N;
     std::cout<<"ha ingresado radio"<<N<<std::endl;
+    int D;
+    D=2*N;
+    std::cout<<"diametro es"<<D<<std::endl;
     return 0;
     
 
